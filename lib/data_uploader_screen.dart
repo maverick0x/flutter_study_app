42db2051fade75b78c745bf14b705c552b88d982
Farouk Bello <mhiztahymodder@gmail.com>
2023-07-13 14:03:05 +0100

    Connected & initialized firebase with the app.

---
+++ lib/data_uploader_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DataUploaderScreen extends StatelessWidget {
+  const DataUploaderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Uploading"),
+      ),
+    );
+  }
+}
